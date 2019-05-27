dad992a29cb27927da5a1fe7beebc6fb373e4bf8
moniet <moniet.sawhney@gmail.com>
2019-05-27 09:42:05 +0100

    added foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190527062404) do
+ActiveRecord::Schema.define(version: 20190527083030) do
 
   create_table "articles", force: :cascade do |t|
     t.string  "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20190527062404) do
   create_table "favourites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "article_id"
   end
 
   create_table "users", force: :cascade do |t|
