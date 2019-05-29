31419db5008763b38827188879b129347d86402f
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-05-29 12:30:07 +0100

    Fixed search function to recognise incorrect user entry

--- app/functions.rb
+++ app/functions.rb
@@ -1,19 +1,18 @@
 # CURATED
 def print_curated_article_overview(user)
   puts "\n\n"
-
   articles = Article.where(curated: true)
   curated_overview = []
   articles.each {|article| curated_overview << "#{article.overview}"}
   puts "\n"
-    puts "-- Choose the article that you would like to read by typing its number --"
-    puts "\n"
-   article_number = gets.chomp.to_i - 1
-   article = articles[article_number]
-   user.article_id = article.id
-   puts "\n"
-   puts article.overview
-   puts "\n"
+  puts "-- Choose the article that you would like to read by typing its number --"
+  puts "\n"
+  article_number = gets.chomp.to_i - 1
+  article = articles[article_number]
+  user.article_id = article.id
+  puts "\n"
+  puts article.overview
+  puts "\n"
 end
 
 def list_curated_articles
@@ -32,11 +31,9 @@ end
 
 # LONGEST OVERVIEW
 def longest_article(user)
-  
   nums = []
   Article.all.each {|article| nums << article.overview.length}
   largest = nums.sort.last
-
   article = Article.all.find {|article| article.overview.length == largest}
   puts "\n"
   puts "-- L O N G E S T  A R T I C L E --"
@@ -44,7 +41,6 @@ def longest_article(user)
   puts article.title.upcase
   puts "\n"
   puts article.overview.gsub("\n","")
-
   user.article_id = article.id
   puts "\n"
 end
@@ -53,12 +49,10 @@ end
 # MOST LIKED
 def most_liked_num
   nums = []
-
   Article.all.each do |article|
     len = Favourite.where(article_id: article.id).size
     nums << len
   end
-
   nums.sort.last
 end
 
@@ -92,21 +86,16 @@ def aiod(user)
   url = "https://api.nasa.gov/planetary/apod?api_key=giSxdlW48Uaffgw7kHUbUnUOkmwUpZijYQhGe5ep"
   uri = URI.parse(url)
   data = Net::HTTP.get(uri)
-
   json = JSON.parse(data)
-
   puts "\n"
   puts "-- A S T R O N O M Y  I N F O  O F  T H E  D A Y --"
   puts "===================================================\n\n"
-
   puts "
     TITLE: #{json["title"].upcase!}'\n
     DATE: #{json["date"]}'\n
     OVERVIEW: \n\n #{json["explanation"]}\n
   "
-  
   new_article = Article.create(title: json["title"], date: json["date"], overview: json["explanation"], curated: false)
-
   user.article_id = new_article.id
   puts "\n"
 end
@@ -114,10 +103,7 @@ end
 def add_to_fav(user)
   article_id = user.article_id
   user_id = user.user_id
-
   user_liked = Favourite.all.find {|fav| fav.article_id == article_id && fav.user_id == user_id}
-
-  #binding.pry
   if !user_liked && article_id
     Favourite.create(article_id: user.article_id, user_id: user.user_id)
     puts "\n"
@@ -141,7 +127,6 @@ def remove_fav(user)
   puts "-- Are you sure you want to remove this article from your favourites? (yes/no) --"
   puts "\n"
   input = gets.chomp
-
   loop do
     if input == "yes"
       fav = Favourite.find_by(article_id: article_id, user_id: user_id)
@@ -173,52 +158,48 @@ def search(user)
   searched_name = gets.chomp
   puts "\n"
   searched_name.downcase!
-  # num1 = rand(1..10) * rand(10)
-  # num2 = num1 + 10
-
   articles = Article.all.select {|article| article.title.downcase.index(searched_name) } 
-
   puts "\n"
-  articles.each_with_index do |article, index|
-    puts "#{index+1}. #{article.title.upcase}\n\n"
-  end
-  puts "\n"
-  puts "-- Choose the article that you would like to read by typing its number --"
-  puts "\n"
+  if !articles.empty?
+    articles.each_with_index do |article, index|
+      puts "#{index+1}. #{article.title.upcase}\n\n"
+    end
+    puts "\n"
+    puts "-- Choose the article that you would like to read by typing its number --"
+    puts "\n"
     article_number = gets.chomp.to_i - 1
     article = articles[article_number]
-
     puts "\n\n"
     puts article.title
     puts "\n"
     puts article.overview
     puts "\n"
     user.article_id = article.id
+    else
+      puts "ERROR 404! No articles found with this search term"
+      input(user)
+  end
+  
 end
 
 
 def favourites(user)
   user_id = user.user_id
   list = Favourite.all.select {|fav| fav.user_id == user_id}
-  #binding.pry
   article_arr = list.map {|fav| Article.find_by(id: fav.article_id)}
-  #article_arr.pop # get rid of nil value
-  #binding.pry
-
   puts "\n"
   puts "-- F A V O U R I T E  A R T I C L E S --"
   puts "========================================\n\n"
-
   article_arr.each_with_index {|article, index| puts "#{index+1}. #{article.title}\n\n"}
-    puts "\n"
-    puts "-- Choose the article that you would like to read by typing its number --"
-    puts "\n"
-    article_number = gets.chomp.to_i - 1
-    article = article_arr[article_number]
-    user.article_id = article.id
-    puts "\n"
-    puts article.overview
-    puts "\n"
+  puts "\n"
+  puts "-- Choose the article that you would like to read by typing its number --"
+  puts "\n"
+  article_number = gets.chomp.to_i - 1
+  article = article_arr[article_number]
+  user.article_id = article.id
+  puts "\n"
+  puts article.overview
+  puts "\n"
 end
 
 def help
@@ -243,7 +224,8 @@ def help
   puts "\n"
 end
 
-
-
-# TODO"'my location is ...' - changes your curent location to a new entry"\n
-# TODO"'from my location' - prints the top 5 objects visible from the choosen location"n
+def choose_by_number
+  puts "\n"
+  puts "-- Choose the article that you would like to read by typing its number --"
+  puts "\n"
+end
